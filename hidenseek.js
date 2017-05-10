6b160387fc4858ad579bca7a15b26c75221b55dd
Анастасия <morina.anastasiya@gmail.com>
2017-05-10 18:52:56 +0300

    Update hidenseek.js

--- hidenseek.js
+++ hidenseek.js
@@ -6,7 +6,8 @@ const Pokemon = require('./pokemon');
 const PokemonList = require('./pokemonList'); 
 
 /* 
-Принимает в качестве аргументов путь и PokemonList. Функция должна «спрятать» в папке, указанной в первом аргументе, случайное число покемонов из списка во втором аргументе. 
+Принимает в качестве аргументов путь и PokemonList. Функция должна «спрятать» в папке, указанной в первом аргументе, 
+случайное число покемонов из списка во втором аргументе. 
 Не более 3. 
 И не более чем передано. 
 Покемоны должны быть выбраны из списка случайным образом. 
@@ -16,49 +17,49 @@ Cоздать 10 папок с именами 01, 02 и так далее.
 Вернуть список спрятанных покемонов. 
 Функция должна быть ассинхронной. Использование колбэка или промиса на ваше усмотрение. 
 */ 
+const random = (min, max) => {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	max = Math.floor(Math.random() * (max - min + 1));
+	return max + min;
+};
 
 const hide = (way, PokemonList, callback) => { 
 
 	if (PokemonList.length <= 3) { 
-		let number = random(1, PokemonList.length); 
+		var number = random(1, PokemonList.length); 
 	} else { 
-		let number = random(1, 3); 
+		var number = random(1, 3); 
 	} 
 
 	var hidePockemons = []; //выбираем любой элемент массива PokemonList и записываем в новый массив 
-	for (let i = 0; i < number; ++i) { 
+	for (let i = 0; i < number; i++) { 
 		var rand = Math.floor(Math.random() * PokemonList.length); 
 		hidePockemons[i] = PokemonList[rand]; 
 		console.log(hidePockemons[i]); 
 	} 
 
 	for (let i = 1; i <= 10; i++) { 
-		let name1 = (i == 10) ? way + i : way + '0' + i; //Cоздать 10 папок с именами 01, 02 и так далее. 
-		fs.mkdir(name1, err => { 
+		let name = (i == 10) ? way + '/' + i : way + '/0' + i; //Cоздать 10 папок с именами 01, 02 и так далее. 
+		fs.mkdir(name, err => { 
 			if (err) throw err; 
-			console.log('Папка ${name1} создана'); 
-			if (i % 2) { //В некоторых из них создать файл pokemon.txt. В папке должен быть только один такой файл. 
-				let name2 = name1 + './pokemon.txt'; 
-				fs.mkdir(name2, err => { 
+			console.log('Папка ' + name + ' создана'); 
+			var count = 0;
+			if (hidePockemons[i-1] instanceof Pokemon) {
+				let textPoc = hidePockemons[i-1].name + '|' + hidePockemons[i-1].level;
+				fs.writeFile(name + '/pokemon.txt', textPoc, err => {
 					if (err) throw err; 
-					console.log('Файл pokemon.txt в папке ${name1} создан'); 
-				} 
+					console.log('Файл pokemon.txt в папке ' + name + ' создан'); 
+					count++;
+					console.log('Покемон ' + textPoc + ' спрятан в файле ' + name + '/pokemon.txt'); 
+					if (count == hidePockemons.length) {
+						callback(PokemonList);
+					}
+				});
 			}
 		})			
 	} 
-
-	
-	for (let i = 0; i < number; i++) { 
-		let textPoc = hidePockemons[i].getName + '|' + hidePockemons[i].getLevel; 
-		fs.writeFile(way + '/pockemon.txt', textPoc, err => { 
-			if (err) throw err; 
-			console.log('Покемон ${textPoc} спрятан в файле ${way} /pockemon.txt'); 
-			if (i == (number - 1)) {
-				callback(hidePockemons);
-			}
-		});
-	}	
-} 
+}
 
 /* 
 Функция seek 
@@ -66,21 +67,27 @@ const hide = (way, PokemonList, callback) => {
 Функция должна быть ассинхронной. Использование колбэка или промиса на ваше усмотрение. 
 */ 
 
+
 const seek = (way, callback) => { 
-	const pokemonList = new PokemonList(); 
-	var count = 0; 
-	fs.readFile(way + '/pockemon.txt', 'utf8', (err, list) => { 
-		if (err) { 
-			throw err; 
-		} else { 
-			let pokemonArray = list.split(", "); 
-			pokemonList.add(pokemonArray[0], pokemonArray[1]); 
-		} 
-	callback(pokemonList); 
-	}); 
+	const pokemonList = new PokemonList();
+	var count = 0;
+	for (let i = 1; i <= 10; i++) {
+        let name = (i == 10) ? way + i : way + '0' + i;
+		fs.readFile(name + '/pokemon.txt', 'utf8', (err, list) => { 
+			if (!err) { 
+				let pokemonArray = list.split(", "); 
+				pokemonList.add(pokemonArray[0], pokemonArray[1]); 
+				console.log(list);
+			} 
+			count++;
+            if (count == 10) {
+                callback(pokemonList);
+            }
+		});
+	}		
 } 
 
 module.exports = { 
-hide, 
-seek 
+	hide, 
+	seek 
 }
