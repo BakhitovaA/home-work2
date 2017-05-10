b7bb6a0a773fa67d912666ab6cc53c55dea267de
Анастасия <morina.anastasiya@gmail.com>
2017-05-10 18:54:35 +0300

    Update pokemonList.js

--- pokemonList.js
+++ pokemonList.js
@@ -3,20 +3,9 @@
 var Pokemon = require('./pokemon');
 
 class PokemonList extends Array {
-	constructor (...pokemons) {
-		return super(...pokemons);
-	}
-
 	add (name, level) {
 		let newPokemon = new Pokemon (name, level);
 		this.push(newPokemon);
-	}
-
-    show(){
-		for (let pokemon of this) {
-			console.log(pokemon.getName + ' ' + pokemon.getLevel);
-		}
-		console.log("Общее количество: " + this.length);
 	}
 }
 
